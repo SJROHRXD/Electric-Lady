34a8918839bfcdb86c94b4577b6ac659e0f3f22d
SJRXD <rohr.olasz@gmail.com>
2023-08-16 03:55:42 -0700

    Formatting renderer.js

--- renderer.js
+++ renderer.js
@@ -1,5 +1,3 @@
-// const information = document.getElementById('info');
-// information.innerText = `This app is using Chrome (v${versions.chrome}), Node.js (v${versions.node}), and Electron (v${versions.electron}).`
-
-const information = document.getElementById('info');
+const information = document.getElementById("info");
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()}).`;
+
