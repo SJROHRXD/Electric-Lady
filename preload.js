84a0e61ceac251cb9c266dae7dd6327310422f1c
SJRXD <rohr.olasz@gmail.com>
2023-08-16 04:00:27 -0700

    Adding IPC Renderer

--- preload.js
+++ preload.js
@@ -4,6 +4,7 @@ const { contextBridge } = require('electron');
 contextBridge.exposeInMainWorld( 'versions', {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron
+    electron: () => process.versions.electron,
+    ping: () => ipcRenderer.invoke('ping')
 });
 
